304d7cc5d1db1a49e49633e9e8f10517d90d2efc
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-21 22:20:27 +0300

    test basics

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "creates a user successifully with valid data" do
+    user=User.create(user_name:"Mercy_k",first_name: "Mercy", last_name: "Kioko",email:"Mercykioko.com",password: "12345",age:25)
+    expect(user).to be_valid
+  end
+
+  describe "validations" do
+    it { is_expected_to validate_presence_of(:user_name) }
+    # it { is_expected_to validate_uniqueness_of(:username) }
+  end
+end
