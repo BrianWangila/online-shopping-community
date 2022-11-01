b8b59d8dd8dc6aa037f09055855b500f68da0fa3
Brian Wangila <bwanjalah@gmail.com>
2022-11-01 22:10:48 +0300

    create searchers controller tests

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do 
-  it 'is found successfully by search_term' do
-    # b = Search.new
+  it 'has create method' do
     search = Search.find_by('search_term': 'search_term')
-    expect(search).to be_truthy
+    expect(search).to be_valid
+  end
+
+  it 'has trends method' do
+
+  end
+
+  it 'has index method' do 
+
   end
 end
