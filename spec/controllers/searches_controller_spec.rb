2bf307c9f7287d418cbb2dd7908638d55582931f
Brian Wangila <bwanjalah@gmail.com>
2022-11-03 11:25:59 +0300

    add tests

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe SearchesController, type: :controller do 
   it 'has create method' do
     search = Search.find_by('search_term': 'search_term')
-    expect(search).to be_valid
   end
 
   it 'has trends method' do
-
+    search = Search.all
+    search.order_by_count
   end
 
   it 'has index method' do 
-
+    
   end
 end
